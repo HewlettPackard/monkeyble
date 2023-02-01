4a0dd73ec673247511fe7354324dd3c34b688457
Nicolas Marcq <nico.marcq@gmail.com>
2023-02-01 10:58:09 +0100

    added  "❌ MONKEYBLE ERROR ❌" in logs output when scenario failed
    make easier to retrieve a failed scenario

--- plugins/module_utils/exceptions.py
+++ plugins/module_utils/exceptions.py
@@ -10,7 +10,8 @@ global_display = Display()
 class MonkeybleException(Exception):
     def __init__(self, message, scenario_description=None, exit_code=1):
         super().__init__(message)
+        global_display.display(msg=f"❌ MONKEYBLE ERROR ❌", color=C.COLOR_ERROR)
         if scenario_description is not None:
-            global_display.display(msg=f"🙊 Monkeyble failed scenario ❌: {scenario_description}", color=C.COLOR_ERROR)
+            global_display.display(msg=f"🙊 Failed scenario: {scenario_description}", color=C.COLOR_ERROR)
         global_display.display(msg=message, color=C.COLOR_ERROR)
         sys.exit(exit_code)
