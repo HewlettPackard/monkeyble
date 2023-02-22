dc2e77f5357e1074f7b4ddaf5428d1a17ee14120
Anthony Belhadj <abelhadj@hpe.com>
2023-02-22 18:34:07 +0100

    Fix message on should_fail

--- plugins/module_utils/exceptions.py
+++ plugins/module_utils/exceptions.py
@@ -10,8 +10,13 @@ global_display = Display()
 class MonkeybleException(Exception):
     def __init__(self, message, scenario_description=None, exit_code=1):
         super().__init__(message)
-        global_display.display(msg=f"❌ MONKEYBLE ERROR ❌", color=C.COLOR_ERROR)
-        if scenario_description is not None:
-            global_display.display(msg=f"🙊 Failed scenario: {scenario_description}", color=C.COLOR_ERROR)
-        global_display.display(msg=message, color=C.COLOR_ERROR)
+        if exit_code == 0:
+            color = C.COLOR_OK
+        else:
+            color = C.COLOR_ERROR
+            global_display.display(msg=f"❌ MONKEYBLE ERROR ❌", color=color)
+            if scenario_description is not None:
+                global_display.display(
+                    msg=f"🙊 Failed scenario: {scenario_description}", color=color)
+        global_display.display(msg=message, color=color)
         sys.exit(exit_code)
