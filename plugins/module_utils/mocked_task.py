c655beb194cb2c8795bc3561352be43849901ec6
Nicolas Marcq <nico.marcq@gmail.com>
2026-04-20 15:10:14 +0200

    fix mock task with Ansible > 2.19

--- plugins/module_utils/mocked_task.py
+++ plugins/module_utils/mocked_task.py
@@ -1,6 +1,8 @@
 from ansible.playbook.task import Task
 
 class MockedTask(Task):
+    """Task subclass that allows setting resolved_action, needed for Ansible 2.19+."""
+
     @property
     def resolved_action(self) -> str | None:
         return super().resolved_action()
@@ -8,10 +10,3 @@ class MockedTask(Task):
     @resolved_action.setter
     def resolved_action(self, action):
         self._resolved_action = action
-
-    @classmethod
-    def from_task(cls, task: Task) -> 'MockedTask':
-        mocked_task = task.copy()
-        mocked_task.__class__ = cls
-
-        return mocked_task
